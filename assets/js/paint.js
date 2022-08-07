fd2ef48129fa9dcb4734d4cbaa2e7dd2b41a4e56
Eric <ericlg.link@gmail.com>
2022-08-06 23:34:50 -0700

    Finished configureListeners function

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,9 +1,10 @@
 function configureListeners() {
-    let images = // select img elements  
+    let images = document.getElementsByTagName('img');// select img elements  
 
 
      for (var i = 0; i < images.length; i++) {        
-        // iterate over images and add mouseover event listeners      
+        document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false);
+        document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity, false); // iterate over images and add mouseover event listeners      
     } 
 }
 
